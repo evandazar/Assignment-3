eb960bc0994a04a782729cbe7236ec5f0dc1ecb4
Valdez <evandervaldez@my.whccd.edu>
2020-01-29 20:46:38 -0800

    finished triangle assignment

--- script2.js
+++ script2.js
@@ -14,20 +14,19 @@ for (let i = 1; i <= 100; i++)
 {
     if (i%5 == 0 && i%3 == 0)
     {
-           console.log("fizzbuzz");
+        console.log("fizzbuzz");
     }  
-     else if (i%3 == 0)
-     {
-         console.log("fizz");
+    else if (i%3 == 0)
+    {
+        console.log("fizz");
         
-     }
-     else if (i%5 == 0)
-     {
-            console.log("buzz");
-     }
-    
-     else
-     {
-         console.log(i);
-     }
+    }
+    else if (i%5 == 0)
+    {
+        console.log("buzz");
+    }
+    else
+    {
+        console.log(i);
+    }
 }
